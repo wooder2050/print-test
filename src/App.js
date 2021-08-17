389f5006ab5eddaf16c8743bed8ad59ac9604a91
zeze <zeze.kang@bunjang.co.kr>
2021-08-17 17:41:40 +0900

    imp: 주소 입력 완성

--- src/App.js
+++ src/App.js
@@ -1,44 +1,43 @@
-import { useState, useCallback, useEffect } from 'react';
-import printJS from 'print-js';
-import styled from 'styled-components';
-import Jsbarcode from 'jsbarcode';
-import './App.css';
+import { useState, useCallback, useEffect } from "react";
+import Jsbarcode from "jsbarcode";
+import "./App.css";
 
 const mockData = {
-  shippingInvoice: '1234 - 1234 - 5678',
-  orderDate: '2020.01.01',
+  shippingInvoice: "1234-1234-5678",
+  orderDate: "2020.01.01",
   printPage: 1,
   reprint: 2,
   divisionBarcode: 1234,
   divisionCode: 8,
   receiver: {
-    name: '홍길동',
-    homePhoneNumber: '02-1234-5678',
-    mobilePhoneNumber: '010-1234-5678',
+    name: "홍길동",
+    homePhoneNumber: "02-1234-5678",
+    mobilePhoneNumber: "010-1234-5678",
     address:
-      '서울 중구 세종대로9길 53 [서소문동 58-12] 홍길동아파트 101동 201호',
-    detailAddress: '홍길동아파트 101동 201호',
+      "서울 중구 세종대로9길 53 [서소문동 58-12] 홍길동아파트 101동 201호",
+    detailAddress: "홍길동아파트 101동 201호",
   },
   invoiceBarcode: 123412345678,
   sender: {
-    name: '송화인',
-    homePhoneNumber: '02-2468-0246',
-    mobilePhoneNumber: '010-2468-0246',
-    address: '서울 중구 태종대로9길 53 [서소문동 58-12]',
-    detailAddress: '홍길동아파트 102동 301호',
+    name: "송화인",
+    homePhoneNumber: "02-2468-0246",
+    mobilePhoneNumber: "010-2468-0246",
+    address: "서울 중구 태종대로9길 53 [서소문동 58-12]",
+    detailAddress: "홍길동아파트 102동 301호",
   },
-  boxInfo: '극소 B 1',
+  boxInfo: "극소 B 1",
   fare: 0,
-  fareInfo: '신용',
+  fareInfo: "신용",
   productName: [
-    '테스트 TEST 상품 정보 ABCDEFG0000 컬러 (COLOR) : 12345BK_블랙',
-    '테스트 TEST 상품 정보 ABCDEFG0001 컬러 (COLOR) : 12345TW_화이트',
+    "테스트 TEST 상품 정보 ABCDEFG0000 컬러 (COLOR) : 12345BK_블랙",
+    "테스트 TEST 상품 정보 ABCDEFG0001 컬러 (COLOR) : 12345TW_화이트",
   ],
-  deliveryAddress: '서소문 58-12 대한통운',
-  deliveryMessage: '안내문구 삽입',
-  deliveryPoint: '중구Bsub',
-  deliveryPerson: '홍길동',
-  deliveryPointShort: 'A01',
+  deliveryAddress: "서소문 58-12 대한통운",
+  deliveryMessage: "안내문구 삽입",
+  deliveryPoint: "중구Bsub",
+  deliveryPerson: "홍길동",
+  deliveryPointShort: "A01",
+  deliveryCode: "K95-3",
 };
 
 function App() {
@@ -46,13 +45,13 @@ function App() {
 
   const handleScan = useCallback(async () => {
     function loadingData() {
-      console.log('loading complete!!');
+      console.log("loading complete!!");
     }
     await loadingData();
     setIsScan(true);
     setTimeout(function () {
       // printJS('print', 'html');
-      const printEl = document.getElementById('print');
+      const printEl = document.getElementById("print");
       const tmp = document.body.innerHTML;
       document.body.innerHTML = printEl.innerHTML;
       window.print();
@@ -61,23 +60,65 @@ function App() {
   }, [setIsScan]);
 
   useEffect(() => {
-    Jsbarcode('#barcode', mockData.invoiceBarcode, {
-      width: 2,
-      height: 40,
+    Jsbarcode("#barcode", mockData.divisionBarcode, {
+      width: 1,
+      height: 56,
       displayValue: false,
+      margin: 0,
+    });
+
+    Jsbarcode("#invoice-barcode", mockData.invoiceBarcode, {
+      width: 1,
+      height: 18,
+      displayValue: false,
+      margin: 0,
     });
   }, []);
 
   return (
-    <div class='app-wrapper'>
+    <div className="app-wrapper">
       {/* {isScan && <PrintPageContainer id="print">test page</PrintPageContainer>} */}
-      <div id='print'>
-        <div class='print-page-container'>
-          <div class='header-wrapper'>
-            <div class='invoice-title'>운송장번호</div>
+      <div id="print">
+        <div className="print-page-container">
+          <div className="header-wrapper">
+            <div className="invoice-title">운송장번호</div>
+            <div className="invoice-text">{mockData.shippingInvoice}</div>
+            <div className="order-date-text">{mockData.orderDate}</div>
+            <div className="print-page-text">{`${mockData.printPage}/${mockData.printPage}`}</div>
+            <div className="reprint-text">{`재출력: ${mockData.reprint}`}</div>
           </div>
-          <div class='barcode-wrapper'>
-            <svg id='barcode' />
+          <div className="invoice-wrapper">
+            <div className="barcode-wrapper">
+              <svg id="barcode" />
+            </div>
+            <div className="division-code-text">{mockData.divisionCode}</div>
+            <div className="delivery-code-text">{mockData.deliveryCode}</div>
+          </div>
+          <div className="address-wrapper">
+            <div className="address-title">받는분</div>
+            <div className="address-content-wrapper">
+              <div className="address-content-sub">
+                <div className="address-content-sub-text">
+                  {mockData.receiver.name}
+                </div>
+                <div className="address-content-phone-number-text">
+                  {mockData.receiver.homePhoneNumber}
+                </div>
+                <div className="address-content-sub-bar">/</div>
+                <div className="address-content-sub-text">
+                  {mockData.receiver.mobilePhoneNumber}
+                </div>
+                <div className="invoice-barcode-wrapper">
+                  <svg id="invoice-barcode" />
+                </div>
+              </div>
+              <div className="address-content-main">
+                {mockData.receiver.address}
+              </div>
+              <div className="address-content-detail">
+                {mockData.receiver.detailAddress}
+              </div>
+            </div>
           </div>
         </div>
       </div>
